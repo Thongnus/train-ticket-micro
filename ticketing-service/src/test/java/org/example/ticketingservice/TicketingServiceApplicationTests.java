71fee00b493c20ae0d84915fcb73358b379939f6
Thong non <bathong448@gmail.com>
2025-11-05 00:39:43 +0700

    push

---
+++ ticketing-service/src/test/java/org/example/ticketingservice/TicketingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ticketingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
